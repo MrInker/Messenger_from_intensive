9de51fa04108c21bd28e09403bfcf4aafc0b99fb
Mr.Inker <leviarius@gmail.ru>
2022-10-05 22:37:45 +0300

    Day_3.1 Branch 2

--- client/chat.py
+++ client/chat.py
@@ -6,42 +6,53 @@ send_message = document.getElementById("send_message")
 sender = document.getElementById("sender")
 message_text = document.getElementById("message_text")
 chat_window = document.getElementById("chat_window")
+emoji_selector = document.getElementById("emoji_selector")
 
 # Добавляет новое сообщение в список сообщений
 def append_message(message):
     # Создаем HTML-элемент представляющий сообщение
     item = document.createElement("li")  # li - это HTML-тег для элемента списка
-    item.className = "list-group-item"   # className - определяет как элемент выглядит
+    item.className = "list-group-item"  # className - определяет как элемент выглядит
     # Добавляем его в список сообщений (chat_window)
     item.innerHTML = f'[<b>{message["sender"]}</b>]: <span>{message["text"]}</span><span class="badge text-bg-light text-secondary">{message["time"]}</span>'
     chat_window.prepend(item)
 
+
 # Вызывается при клике на send_message
 async def send_message_click(e):
     # Отправляем запрос
-    await fetch(f"/send_message?sender={sender.value}&text={message_text.value}", method="GET")
+    sender_full_name = emoji_selector.value + sender.value
+    await fetch(f"/send_message?sender={sender_full_name}&text={message_text.value}", method="GET")
     # Очищаем поле
     message_text.value = ""
 
 
 # Загружает новые сообщения с сервера и отображает их
 async def load_fresh_messages():
-    # 1. Загружать все сообщения каждую секунду (большой трафик)
-    result = await fetch("/get_messages", method="GET")  # Делаем запрос
-    chat_window.innerHTML = ""  # Очищаем окно с сообщениями
+    global last_seen_id
+    # БЫЛО 1. Загружать все сообщения каждую секунду (большой трафик)
+    # СТАЛО 2. Загружать только новые сообщения
+    result = await fetch(f"/get_messages?after={last_seen_id}", method="GET")  # Делаем запрос
+    # chat_window.innerHTML = ""  # Очищаем окно с сообщениями
     data = await result.json()
     all_messages = data["messages"]  # Берем список сообщений из ответа сервера
     for msg in all_messages:
+        last_seen_id = msg["msg_ID"]
         append_message(msg)
-    set_timeout(1, load_fresh_messages) # Запускаем загрузку заново через секунду
-    # 2. Загружать только новые сообщения
+
+    set_timeout(1, load_fresh_messages)  # Запускаем загрузку заново через секунду
 
 
+async def message_keypress(event):  # срабатываение на Emter
+    if event.keyCode == 13:
+        await send_message_click(event)
+
+
+
+message_text.onkeypress = message_keypress
 # Устнаваливаем действие при клике
 send_message.onclick = send_message_click
-append_message({"sender":"MIKE", "text":"Add Message Works!!!", "time": "25:88"})
-load_fresh_messages()
-
+load_fresh_messages()  # начальная загрузка сообщений
 
 # from utils import set_timeout, fetch
 #
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 app = Flask(__name__, static_folder="./client", template_folder="./client")  # Настройки приложения
 
+msg_ID = 1
 all_messages = []
 
 
@@ -12,18 +13,23 @@ def chat_page():
 
 
 def add_message(sender, text):
+    global msg_ID
     new_message = {
         "sender": sender,
         "text": text,
-        "time": datetime.now().strftime("%H:%M")
+        "time": datetime.now().strftime("%H:%M"),
+        "msg_ID": msg_ID
     }
+    msg_ID += 1
     all_messages.append(new_message)
 
 
 # API для получения списка сообщений
+# будем просить сервер дать только новые сообщения /get_messages?after=0 /get_messages?after=5
 @app.route("/get_messages")
 def get_messages():
-    return {"messages": all_messages}
+    after = int(request.args['after'])
+    return {"messages": all_messages[after:]}  # Делаем СЛАЙС - отрезать кусочек. Взять всё после сообщения after
 
 
 # API для получения отправки сообщения  /send_message?sender=Mike&text=Hello
@@ -54,5 +60,5 @@ def info_page():
     info = len(all_messages)
     return f"<br><br><center><h1> Количество сообщений: {info} <br> <a href=\"/chat\">Chat</a> </h1></center>"
 
-
-app.run()
+# app.run() по умолчанию - запускается локально на порту 5000 (127,0,0,1)
+app.run() # 80 стандартный порт для ХТТП, что бы не вводить порт
